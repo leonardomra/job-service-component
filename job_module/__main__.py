1e4c1b2198f47a45a453f3e5153d61bda623e53c
Leonardo de Araújo <dearaujo@uni-bremen.de>
2020-07-25 06:44:39 +0200

    docker setup

--- job_module/__main__.py
+++ job_module/__main__.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
 import connexion
-
+import os
 from job_module import encoder
 
 
 def main():
+    print(os.environ['INIT'])
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Job.ai API'}, pythonic_params=True)

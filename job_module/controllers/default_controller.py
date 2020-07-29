04fae0a0a3fc9edec270ec480dded297152b1868
Leonardo de Araújo <dearaujo@uni-bremen.de>
2020-07-29 04:06:34 +0200

    health check

---
+++ job_module/controllers/default_controller.py
@@ -0,0 +1,16 @@
+import connexion
+import six
+
+from job_module.models.health import Health  # noqa: E501
+from job_module import util
+
+
+def health_get():  # noqa: E501
+    """health_get
+
+    Check health of service. # noqa: E501
+
+
+    :rtype: Health
+    """
+    return 'Job Service Component is up!'

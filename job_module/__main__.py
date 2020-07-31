0e1365eeda50f8310672ff6007e6d3981edfc9b2
Leonardo de Araújo <dearaujo@uni-bremen.de>
2020-07-31 22:09:34 +0200

    debug setup from env.

--- job_module/__main__.py
+++ job_module/__main__.py
@@ -10,7 +10,7 @@ def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Job.ai API'}, pythonic_params=True)
-    app.run(port=80, debug=True)
+    app.run(port=80, debug= bool(os.environ['DEBUG']))
 
 if __name__ == '__main__':
     main()

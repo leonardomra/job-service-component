2216e7012d62a7e3a5a27b79118b28e0a9bb263a
Leonardo de Araújo <dearaujo@uni-bremen.de>
2020-08-17 13:50:52 +0200

    orcomm lib updated to latest

--- orcomm_module/orcommunicator.py
+++ orcomm_module/orcommunicator.py
@@ -6,13 +6,47 @@ import boto3
 
 class ORCommunicator():
     
-    def __init__(self, AWS_REGION, AWS_ACCESS_KEY, AWS_SECRET_KEY):
-        client = boto3.client(
-            'sns',
-            region_name=AWS_REGION,
-            aws_access_key_id=AWS_ACCESS_KEY,
-            aws_secret_access_key=AWS_SECRET_KEY
-        )
-        self.topic = ORTopic(client)
-        self.channel = ORChannel(client)
-        self.queue = ORQueue(client)
+    def __init__(self, AWS_REGION=None, AWS_ACCESS_KEY=None, AWS_SECRET_KEY=None):
+        self.awsRegion = AWS_REGION
+        self.awsAccessKey = AWS_ACCESS_KEY
+        self.awsSecretKey = AWS_SECRET_KEY
+        self.topics = {}
+        self.channels = {}
+        self.queues = {}
+        
+    def addTopic(self, topicName=None, topicArn=None):
+        topic = ORTopic(self.awsRegion, self.awsAccessKey, self.awsSecretKey, topicArn)
+        self.topics[topicArn] = topic
+
+    def addChannel(self, channelName):
+        channel = ORChannel(channelName)
+        self.channels[channelName] = channel
+
+    def addQueue(self, queueName=None, queueArn=None):
+        queue = ORQueue(self.awsRegion, self.awsAccessKey, self.awsSecretKey, queueName)
+        self.queues[queueArn] = queue
+
+    def getTopics(self):
+        return self.topics
+
+    def getChannels(self):
+        return self.channels
+
+    def getQueues(self):
+        return self.queues
+
+    def itemsForQueue(self, queueName, queueArn, messageAttributeNames=[], limit=1, deleteMsgs=False):
+        if queueArn not in self.queues:
+            self.queues[queueArn] = ORQueue(self.awsRegion, self.awsAccessKey, self.awsSecretKey, queueName)
+        return self.queues[queueArn].pullItems(messageAttributeNames, limit, deleteMsgs)
+
+    def getTopic(self, topicArn):
+        return self.topics[topicArn]
+
+    def getChannel(self, channelName):
+        return self.channels[channelName]
+
+    def getQueue(self, queueArn):
+        return self.queues[queueArn]
+
+    
